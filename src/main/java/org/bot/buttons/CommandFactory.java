07747af8b957468d4743c34321b7814680c6b103
oleg-laizik <oleglaizik@gmail.com>
2023-05-29 00:12:22 +0200

    Fix CommandFactory

--- src/main/java/org/bot/buttons/CommandFactory.java
+++ src/main/java/org/bot/buttons/CommandFactory.java
@@ -2,20 +2,24 @@ package org.bot.buttons;
 
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CommandFactory {
     public static InlineKeyboardMarkup buttons(List<String> buttons) {
         return InlineKeyboardMarkup.builder()
-                .keyboardRow(
+                .keyboard(
                         buttons.stream()
                                 .map(buttonName -> {
                                     String[] nameAndText = buttonName.split(":");
-                                    return InlineKeyboardButton.builder()
+                                    InlineKeyboardButton button = InlineKeyboardButton.builder()
                                             .text(nameAndText[1])
                                             .callbackData(nameAndText[0])
                                             .build();
+                                    return Collections.singletonList(button);
+
                                 })
                                 .collect(Collectors.toList())
                 )
@@ -24,13 +28,14 @@ public class CommandFactory {
 
     public static InlineKeyboardMarkup dataButtons(List<String> buttons) {
         return InlineKeyboardMarkup.builder()
-                .keyboardRow(buttons.stream()
+                .keyboard(buttons.stream()
                         .map(buttonName -> {
                             String[] nameAndText = buttonName.split(":");
-                            return InlineKeyboardButton.builder()
+                            InlineKeyboardButton button = InlineKeyboardButton.builder()
                                     .text(nameAndText[1])
                                     .callbackData(buttonName)
                                     .build();
+                            return Collections.singletonList(button);
                         })
                         .collect(Collectors.toList())
                 )
